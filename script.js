a6757aeec18816d1fa6b2160967c5d9286b6cbfb
Simvuyele2013 <124438571+Simvuyele2013@users.noreply.github.com>
2023-02-22 11:36:13 +0200

    JS integration

---
+++ script.js
@@ -0,0 +1,7 @@
+function init() {
+    document.getElementById('fullname').innerHTML = 'Simvuyele Mabaleka';
+    document.getElementById("biography").innerHTML = "Hello! And welcome to my page. I am Simvuyele Mabaleka, a budding web developer and love all things tech.\n\nI am married to a fellow techie and we live in Rustenburg with our toddler son.  I worked as a crime journalist before moving into\n\nCorporate Communications. I am currently transitioning over to web development as the next adventure in my work life.";
+    document.getElementById("address").innerHTML = "48 Stormvoel Avenue,\nCashan,\nRustenburg,\n0299";
+}
+
+init();
